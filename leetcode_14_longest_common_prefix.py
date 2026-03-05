281871b4a85349b80da2f7d0a1bcafc1e5bf3f64
Aishwarya M N <aishwaryamn168@gmail.com>
2026-03-05 07:30:09 +0530

    Implement longest common prefix function

---
+++ leetcode_14_longest_common_prefix.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix=strs[0]
+        for word in strs[1:]:
+            while word.find(prefix)!=0:
+                prefix=prefix[:-1]
+                if prefix=="":
+                    return ""
+        return prefix     
+        
