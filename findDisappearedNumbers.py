70c8237eed00e8d033a0d326db31d34731569170
Aishwarya M N <aishwaryamn168@gmail.com>
2026-03-26 18:08:45 +0530

    Implement function to find disappeared numbers

---
+++ findDisappearedNumbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        present=set(nums)
+        result=[]
+        for i in range(1,len(nums)+1):
+            if i not in present:
+                result.append(i)
+        return result
