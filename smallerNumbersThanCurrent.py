842612f614d36e51de04a6426570b01b86582df2
Aishwarya M N <aishwaryamn168@gmail.com>
2026-03-27 22:46:33 +0530

    Add Solution class for counting smaller numbers
    Implement a solution to count smaller numbers than current.

---
+++ smallerNumbersThanCurrent.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums=sorted(nums)
+        rank={}
+        for i,num in enumerate(sorted_nums):
+            if num not in rank:
+                rank[num]=i
+        result=[]
+        for num in nums:
+                result.append(rank[num])
+        return result        
+
+        
