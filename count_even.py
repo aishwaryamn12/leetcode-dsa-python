456c32e1ab0de53b1bdc314dbaf3f582a600d22c
Aishwarya M N <aishwaryamn168@gmail.com>
2026-03-28 22:19:11 +0530

    Add function to count even numbers in an array

---
+++ count_even.py
@@ -0,0 +1,12 @@
+def count_even(arr):
+    count = 0
+    
+    for num in arr:
+        if num % 2 == 0:
+            count += 1
+    
+    return count
+
+
+arr = [1, 2, 3, 4, 5, 6]
+print(count_even(arr))
