685f13a040a06c91f8584f19161394aaec7e87de
Aishwarya M N <aishwaryamn168@gmail.com>
2026-04-10 00:00:14 +0530

    Add element search functionality in Element.py

---
+++ Element.py
@@ -0,0 +1,13 @@
+arr = [5, 10, 15, 20, 25]
+target = 15
+
+found = False
+
+for i in range(len(arr)):
+    if arr[i] == target:
+        print("Element found at index:", i)
+        found = True
+        break
+
+if not found:
+    print("Element not found")
