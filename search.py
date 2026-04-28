9ad2ca4debacdef1a3199da2d9a57890dfff1dd7
Aishwarya M N <aishwaryamn168@gmail.com>
2026-04-28 22:07:33 +0530

    Implement binary search function in search.py

---
+++ search.py
@@ -0,0 +1,23 @@
+def search(nums, target):
+    l, r = 0, len(nums) - 1
+    
+    while l <= r:
+        m = (l + r) // 2
+        
+        if nums[m] == target:
+            return m
+        
+        # left sorted
+        if nums[l] <= nums[m]:
+            if nums[l] <= target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        else:
+            # right sorted
+            if nums[m] < target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+                
+    return -1
