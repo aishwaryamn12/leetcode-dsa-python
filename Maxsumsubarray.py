cb2142688ff9ee79e4e4d88fcf03a2877b683dbb
Aishwarya M N <aishwaryamn168@gmail.com>
2026-04-30 21:46:46 +0530

    Implement max sum subarray function

---
+++ Maxsumsubarray.py
@@ -0,0 +1,10 @@
+def maxSumSubarray(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+    
+    for i in range(k, len(nums)):
+        window_sum += nums[i]      # add next
+        window_sum -= nums[i-k]    # remove previous
+        max_sum = max(max_sum, window_sum)
+    
+    return max_sum
