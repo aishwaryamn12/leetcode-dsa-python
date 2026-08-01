3634d46c2b10407ba205b9184a96c75a7e2de850
Aishwarya M N <aishwaryamn168@gmail.com>
2026-08-01 19:41:27 +0530

    Add subarray sum solution using prefix sums
    Implement a solution to find the number of subarrays that sum to a given value k using prefix sums.

---
+++ subarray_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sum=0
+        count=0
+        prefix_map={0:1}
+        for num in nums:
+            prefix_sum+=num
+            if prefix_sum - k in prefix_map:
+                count+=prefix_map[prefix_sum-k]
+            if prefix_sum in prefix_map:
+                prefix_map[prefix_sum]+=1
+            else:
+                prefix_map[prefix_sum]=1
+        return count           
+
