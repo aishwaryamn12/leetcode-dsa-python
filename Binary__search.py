db8355b1b01f0348825601663647585f4b4a9ff2
Aishwarya M N <aishwaryamn168@gmail.com>
2026-09-14 22:06:43 +0530

    Implement binary search algorithm in Solution class

---
+++ Binary__search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def binary_search(self, arr, target):
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid] == target:
+                return mid
+
+            elif arr[mid] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return -1
